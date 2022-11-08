fa754596beaa6086104754f1704258c8b5c55598
Serge Moraru <sergemoraru@gmail.com>
2022-11-08 17:57:10 -0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import app from './server';
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+}
+);
---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express = require('express');
+import router from './router';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log('hello world');
+    res.status(200)
+    res.json({ message: 'hello world' });
+});
+
+app.use('/api', router);
+
+export default app;
