95a4ad72a942e38af2e2731bbe3368d337d55ac9
Serge Moraru <sergemoraru@gmail.com>
2022-11-14 17:59:05 -0500

    added product and update handlers and middleware

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,9 @@
 import {Router} from 'express';
+import { body, oneOf, validationResult } from 'express-validator';
+import { title } from 'process';
+import { createProduct, deleteProduct, getOneProduct, getProducts } from './handlers/product';
+import { createUpdate, deleteUpdate, getOneUpdate, getUpdates, updateUpdate } from './handlers/update';
+import { handleInputErrors } from './modules/middleware';
 
 const router = Router();
 
@@ -6,24 +11,32 @@ const router = Router();
  * Product
  */
 
-router.get('/product', (req, res) => {
-    res.status(200);
-    res.json({ message: 'hello world' });
+router.get('/product', getProducts)
+router.get('/product/:id', getOneProduct)
+router.put('/product/:id', body('name').isString(), handleInputErrors, (req, res) => {
 })
-router.get('/product/:id', () => {})
-router.put('/product/:id', () => {})
-router.post('/product', () => {})
-router.delete('/product/:id', () => {})
+router.post('/product',  body('name').isString(), handleInputErrors, createProduct)
+router.delete('/product/:id', deleteProduct)
 
 /**
  * Update
  */
 
-router.get('/update', () => {})
-router.get('/update/:id', () => {})
-router.put('/update/:id', () => {})
-router.post('/update', () => {})
-router.delete('/update/:id', () => {})
+router.get('/update', getUpdates)
+router.get('/update/:id', getOneUpdate)
+router.put('/update/:id', 
+    body('title').optional(), 
+    body('body').optional(), 
+    body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRICATED']).optional(),
+    body('verison').optional(), 
+    updateUpdate   
+    )
+router.post('/update', 
+    body('title').exists().isString(), 
+    body('body').exists().isString(),
+    body('productId').exists().isSemVer(),
+createUpdate)
+router.delete('/update/:id', deleteUpdate)
 
 /**
  * Update Point
@@ -31,8 +44,16 @@ router.delete('/update/:id', () => {})
 
 router.get('/update-point', () => {})
 router.get('/update-point/:id', () => {})
-router.put('/update-point/:id', () => {})
-router.post('/update-point', () => {})
+router.put('/update-point/:id', 
+    body('name').optional().isString, 
+    body('description').optional().isString, 
+    () => {}
+)
+router.post('/update-point', 
+    body('name').isString, 
+    body('description').isString,
+    body('updateId').exists().isString(),
+    () => {})
 router.delete('/update-point/:id', () => {})
 
 export default router;
